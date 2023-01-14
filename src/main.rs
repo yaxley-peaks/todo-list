793f1f759dc3edf77e0962b5401276b7d870df99
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-01-14 18:40:22 +0530

    print todos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use ncurses::*;
 fn main() {
-    println!("Hello, world!");
+    initscr();
+    // addstr("Hello world");
+    refresh();
+
+    let mut quit = false;
+    let mut todos = vec!["Write app", "Learn rust", "Get 1 egg"];
+
+    while !quit {
+        for (row, todo) in todos.iter().enumerate() {
+            mv(row as i32, 0);
+            addstr(*todo);
+        }
+        let key = getch();
+        match key as u8 as char {
+            'q' => quit = true,
+            _ => {}
+        }
+    }
+
+    endwin();
 }
