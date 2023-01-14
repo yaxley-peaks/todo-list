03a058557e67d7d80ed6eb99000f653cc914a8f5
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-01-15 01:34:07 +0530

    can do stuff now

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ type Id = usize;
 
 #[derive(Default)]
 struct Ui {
+    key: Option<i32>,
     list_curr: Option<Id>,
     row: usize,
     col: usize,
@@ -25,18 +26,19 @@ impl Ui {
         self.list_curr = Some(id);
     }
 
-    fn list_element(&mut self, label: &str, id: Id) {
+    fn list_element(&mut self, label: &str, id: Id) -> bool {
         let id_curr = self
             .list_curr
             .expect("Can't create elements outside of lists");
         self.label(
-            &format!("- [ ] {}", label),
+            label,
             if id_curr == id {
                 HIGHLIGHT_PAIR
             } else {
                 REGULAR_PAIR
             },
         );
+        false
     }
 
     fn end_list(&mut self) {
@@ -67,29 +69,33 @@ fn main() {
     refresh();
 
     let mut quit = false;
-    let todos = vec![
+    let mut todos = vec![
         "Write app".to_string(),
         "Learn rust".to_string(),
         "Get 1 egg".to_string(),
-        "Kill Myself".to_string(),
     ];
     let mut todo_curr: usize = 1;
-    let mut dones = vec!["Wake up".to_string(), "Cry".to_string()];
+    let mut dones = vec![
+        "Wake up".to_string(),
+        "Cry".to_string(),
+        "Kill Myself".to_string(),
+    ];
     let mut done_curr = 0usize;
 
     let mut ui = Ui::default();
     while !quit {
+        erase();
         ui.begin(1, 1);
         {
             ui.begin_list(todo_curr);
             for (index, todo) in todos.iter().enumerate() {
-                ui.list_element(todo, index);
+                ui.list_element(&format!("- [ ] {todo}"), index);
             }
             ui.end_list();
             ui.label("--------------------------", REGULAR_PAIR);
-            ui.begin_list(done_curr);
+            ui.begin_list(0);
             for (index, done) in dones.iter().enumerate() {
-                ui.list_element(done, index);
+                ui.list_element(&format!("- [x] {done}"), index + 1);
             }
             ui.end_list();
         }
@@ -103,7 +109,14 @@ fn main() {
                     todo_curr -= 1;
                 }
             }
-            's' => todo_curr = std::cmp::min(todo_curr + 1, todos.len() - 1),
+            's' => if todo_curr + 1 < todos.len() {
+                todo_curr += 1;
+            },
+            '\n' => {
+                if todo_curr < todos.len() {
+                    dones.push(todos.remove(todo_curr));
+                }
+            },
             _ => {}
         }
     }
