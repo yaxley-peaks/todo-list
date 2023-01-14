d52cdf918d44f6bde0ba51b913fb9383b69b4453
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-01-14 20:32:22 +0530

    extracted ui

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,100 @@
-use ncurses::*;
+#![allow(unused)]
 
+use std::default;
+
+use ncurses::*;
 const REGULAR_PAIR: i16 = 0;
 const HIGHLIGHT_PAIR: i16 = 1;
+
+type Id = usize;
+
+#[derive(Default)]
+struct Ui {
+    list_curr: Option<Id>,
+    row: usize,
+    col: usize,
+}
+
+impl Ui {
+    fn begin(&mut self, row: usize, col: usize) {
+        self.col = col;
+        self.row = row;
+    }
+    fn begin_list(&mut self, id: Id) {
+        assert!(self.list_curr.is_none());
+        self.list_curr = Some(id);
+    }
+
+    fn list_element(&mut self, label: &str, id: Id) {
+        let id_curr = self
+            .list_curr
+            .expect("Can't create elements outside of lists");
+        self.label(
+            &format!("- [ ] {}", label),
+            if id_curr == id {
+                HIGHLIGHT_PAIR
+            } else {
+                REGULAR_PAIR
+            },
+        );
+    }
+
+    fn end_list(&mut self) {
+        self.list_curr = None;
+    }
+    fn end(&mut self) {
+        // todo!()
+    }
+
+    fn label(&mut self, label: &str, pair: i16) {
+        mv(self.row as i32, self.col as i32);
+        attron(COLOR_PAIR(pair));
+        addstr(label);
+        attroff(COLOR_PAIR(pair));
+        self.row += 1;
+        // todo!()
+    }
+}
 
 fn main() {
     initscr();
     noecho();
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
     start_color();
     init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
     init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
-    // init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
 
-    // addstr("Hello world");
     refresh();
 
     let mut quit = false;
-    let todos = vec!["Write app", "Learn rust", "Get 1 egg"];
+    let todos = vec![
+        "Write app".to_string(),
+        "Learn rust".to_string(),
+        "Get 1 egg".to_string(),
+        "Kill Myself".to_string(),
+    ];
     let mut todo_curr: usize = 1;
+    let mut dones = vec!["Wake up".to_string(), "Cry".to_string()];
+    let mut done_curr = 0usize;
+
+    let mut ui = Ui::default();
     while !quit {
-        for (index, todo) in todos.iter().enumerate() {
-            let pair = {
-                if todo_curr == index {
-                    HIGHLIGHT_PAIR
-                } else {
-                    REGULAR_PAIR
-                }
-            };
-            attron(COLOR_PAIR(pair));
-            mv(index as i32, 0);
-            addstr(*todo);
-            attroff(COLOR_PAIR(pair));
+        ui.begin(1, 1);
+        {
+            ui.begin_list(todo_curr);
+            for (index, todo) in todos.iter().enumerate() {
+                ui.list_element(todo, index);
+            }
+            ui.end_list();
+            ui.label("--------------------------", REGULAR_PAIR);
+            ui.begin_list(done_curr);
+            for (index, done) in dones.iter().enumerate() {
+                ui.list_element(done, index);
+            }
+            ui.end_list();
         }
+        ui.end();
+
         let key = getch();
         match key as u8 as char {
             'q' => quit = true,
